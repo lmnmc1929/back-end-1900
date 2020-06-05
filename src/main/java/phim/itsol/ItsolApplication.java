b8cf610d6e2da7dad40bf1cec6ec3ab82a83c831
nghia-code1805 <you@example.com>
2020-06-05 10:56:44 +0700

    Project

---
+++ src/main/java/phim/itsol/ItsolApplication.java
@@ -0,0 +1,13 @@
+package phim.itsol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/phim/itsol/ItsolApplicationTests.java
@@ -0,0 +1,13 @@
+package phim.itsol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
