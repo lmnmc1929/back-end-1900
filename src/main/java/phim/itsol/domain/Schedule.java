a7dc97d41412a0898b669d72d97ebed8eb9fa982
minhdi113 <minhpmhe130249@fpt.edu.vn>
2020-06-08 14:50:16 +0700

    schedule

--- src/main/java/phim/itsol/domain/Schedule.java
+++ src/main/java/phim/itsol/domain/Schedule.java
@@ -1,4 +1,35 @@
 package phim.itsol.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "SCHEDULE")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SCHEDULE_SEQ")
+    @SequenceGenerator(sequenceName = "SCHEDULE_SEQ", allocationSize = 1, name = "SCHEDULE_SEQ")
+    @Column(name = "PRODUCT_ID")
+    private Long scheduleId;
+
+    @OneToOne(targetEntity = Room.class)
+    private Room room;
+
+    @OneToOne(targetEntity = Movie.class)
+    private Movie movie;
+
+    @Column(name = "START_TIME")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Column(name = "PRICE_MOVIE")
+    private double priceMovie;
 }
