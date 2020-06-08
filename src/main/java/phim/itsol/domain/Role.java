7d3fb3a296fccd08c5d57d00cf8a9b568d85f5b9
nghia-code1805 <you@example.com>
2020-06-08 11:40:11 +0700

    lan 2

--- src/main/java/phim/itsol/domain/Role.java
+++ src/main/java/phim/itsol/domain/Role.java
@@ -1,4 +1,26 @@
 package phim.itsol.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ROLE")
 public class Role {
+
+    @OneToMany(mappedBy = Manager.class)
+    private List<Manager> managerList;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ROLE_SEQ")
+    @SequenceGenerator(sequenceName = "ROLES_SEQ", allocationSize = 1,name = "ROLE_SEQ")
+    @Column(name = "ROLE_ID")
+    private Long roleId;
+
+    @Column(name = "ROLE_NAME")
+    private String roleName;
 }
